cc550ee0efdc09458046f8ec1a8e08baa7d3b249
Roberto Sousa <robert6257@gmail.com>
2022-03-11 09:16:50 -0300

    Criado a classe Product

--- CRUDProducts/CRUDProducts/Data/CRUDContext.cs
+++ CRUDProducts/CRUDProducts/Data/CRUDContext.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using CRUDProducts.Model;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,11 @@ namespace CRUDProducts.Data
 {
     public class CRUDContext : DbContext
     {
+        public DbSet<Product> Products { set; get; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ CRUDProducts/CRUDProducts/Model/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDProducts.Model
+{
+    public class Product
+    {
+        public int Id { get; set; } 
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
