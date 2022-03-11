88d92fcecd019d3d3155e7b529c3a976a7821af2
Roberto Sousa <robert6257@gmail.com>
2022-03-11 09:31:08 -0300

    Adicionado a migration inicial

---
+++ CRUDProducts/CRUDProducts/Migrations/202203111229377_Migration Initial.cs
@@ -0,0 +1,29 @@
+﻿namespace CRUDProducts.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MigrationInitial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Code = c.String(),
+                        Description = c.String(),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Quantity = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Products");
+        }
+    }
+}
