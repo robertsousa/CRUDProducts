c59b934c886c4661b3d069781bd8b3d491512593
Roberto Sousa <robert6257@gmail.com>
2022-03-11 09:38:50 -0300

    Adicionado a função SelectAll no ProductRepository

---
+++ CRUDProducts/CRUDProducts/Repository/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using CRUDProducts.Data;
+using CRUDProducts.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDProducts.Repository
+{
+    public class ProductRepository
+    {
+
+        public Task<List<Product>> SelectAll()
+        {
+            return Task.Run(() =>
+            {
+                using (CRUDContext context = new CRUDContext())
+                {
+                    return context.Products.ToList();
+                }
+            });
+        }
+    }
+}
